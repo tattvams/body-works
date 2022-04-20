e20f6f243abb74afb36a727e37a205d36d2f9e54
tattvam <tattvams@gmail.com>
2022-04-20 13:27:12 +0530

    second commit

--- lib/custom_drawer/home_drawer.dart
+++ lib/custom_drawer/home_drawer.dart
@@ -94,7 +94,7 @@ class _HomeDrawerState extends State<HomeDrawer> {
                             ),
                             child: ClipRRect(
                               borderRadius: const BorderRadius.all(Radius.circular(60.0)),
-                              child: Image.asset('assets/images/userImage.png'),
+                              child: Image.asset('assets/images/harshit.jpeg'),
                             ),
                           ),
                         ),
